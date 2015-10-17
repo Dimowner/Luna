777fd6c1e4099ea9c7c9c3c1a233f5cdc210ac86
Vitaly Sulimov <quarry386@fastmail.com>
2015-10-17 17:05:49 +0500

    Task Constants utils done

---
+++ app/src/main/java/com/hochland386/luna/utils/Constants.java
@@ -0,0 +1,20 @@
+package com.hochland386.luna.utils;
+
+/**
+ * Created by vitaly on 10/17/15.
+ * This class provides constants for whole application.
+ */
+public class Constants {
+
+//    Public static final fields
+    public static final int LOCATION_TIMEOUT = 45000; /* milliseconds */
+    public static final int COUNT_DOWN_INTERVAL = 1000; /* milliseconds */
+    public static final long LOCATION_MIN_TIME = 0;
+    public static final float LOCATION_MIN_DISTANCE = 0;
+    public static final double DEFAULT_LATITUDE = 0.0;
+    public static final double DEFAULT_LONGITUDE = 0.0;
+
+//    Make default constructor private
+    private Constants() {
+    }
+}
