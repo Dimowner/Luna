a0819608fee6fa9600d6783bde0a40185aa6a5bd
Vitaly Sulimov <quarry386@fastmail.com>
2015-10-19 04:49:26 +0500

    CurrentWeather: Added helper methods for better integration between model and controller

--- app/src/main/java/com/hochland386/luna/model/CurrentWeather.java
+++ app/src/main/java/com/hochland386/luna/model/CurrentWeather.java
@@ -22,6 +22,62 @@ public class CurrentWeather extends Weather {
 
 //    CurrentWeather-specific getters
     /**
+     * Returns true if temperature is less than 0 degree
+     * @return boolean isTemperatureNegative
+     */
+    public boolean isTemperatureNegative() {
+        return getTemperature() < 0;
+    }
+
+    /**
+     * Returns reversed temperature value (e.g. -16 degree will be returned as 16 degree)
+     * @return int reversedTemperature
+     */
+    public int getReversedTemperature() {
+        return getTemperature() * -1;
+    }
+
+    /**
+     * Returns temperature value as String
+     * @return String temperature
+     */
+    public String getTemperatureAsString() {
+        return String.valueOf(getTemperature());
+    }
+
+    /**
+     * Returns reversedTemperature value as String
+     * @return String reversedTemperature
+     */
+    public String getReversedTemperatureAsString() {
+        return String.valueOf(getReversedTemperature());
+    }
+
+    /**
+     * Returns humidity value as String
+     * @return String humidity
+     */
+    public String getHumidityAsString() {
+        return String.format("%s",getHumidity() + "%");
+    }
+
+    /**
+     * Returns pressureInHpa value as String
+     * @return String pressureInHpa
+     */
+    public String getPressureInHpaAsString() {
+        return String.valueOf(getPressureInHpa());
+    }
+
+    /**
+     * Returns pressureInMmhg value as String
+     * @return String pressureInMmhg
+     */
+    public String getPressureInMmhgAsString() {
+        return String.valueOf(getPressureInMmhg());
+    }
+
+    /**
      * Returns place
      * @return String place
      */
