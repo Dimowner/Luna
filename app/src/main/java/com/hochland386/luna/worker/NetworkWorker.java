b56a261710f4bcbbf6007419c7e00c0fdd49d298
Vitaly Sulimov <quarry386@fastmail.com>
2015-11-05 04:20:34 +0500

    NetworkWorker: Implemented downloadForecastWeatherData()

--- app/src/main/java/com/hochland386/luna/worker/NetworkWorker.java
+++ app/src/main/java/com/hochland386/luna/worker/NetworkWorker.java
@@ -2,6 +2,8 @@ package com.hochland386.luna.worker;
 
 import com.hochland386.luna.bus.CurrentWeatherFailureEvent;
 import com.hochland386.luna.bus.CurrentWeatherResponseEvent;
+import com.hochland386.luna.bus.ForecastWeatherFailureEvent;
+import com.hochland386.luna.bus.ForecastWeatherResponseEvent;
 import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
@@ -54,6 +56,29 @@ public class NetworkWorker {
             public void onResponse(Response response) throws IOException {
                 String responseAsString = response.body().string();
                 EventBus.getDefault().post(new CurrentWeatherResponseEvent(responseAsString));
+            }
+        });
+    }
+
+    /**
+     * Downloads data from passed URL in background thread. ForecastWeatherResponseEvent will be
+     * posted when data is ready or ForecastWeatherFailureEvent if any error occurs
+     * @param url URL
+     */
+    public void downloadForecastWeatherData(String url) {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        mClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                EventBus.getDefault().post(new ForecastWeatherFailureEvent());
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                String responseAsString = response.body().string();
+                EventBus.getDefault().post(new ForecastWeatherResponseEvent(responseAsString));
             }
         });
     }
