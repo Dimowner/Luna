349733cab7ad59097735009c931436c033ba4cfc
Vitaly Sulimov <quarry386@fastmail.com>
2015-11-05 02:58:48 +0500

    Implemented CurrentWeatherResponseEvent

---
+++ app/src/main/java/com/hochland386/luna/bus/CurrentWeatherResponseEvent.java
@@ -0,0 +1,24 @@
+package com.hochland386.luna.bus;
+
+/**
+ * Created by vitaly on 11/5/15.
+ */
+public class CurrentWeatherResponseEvent {
+
+//    Members
+    private String mResponse;
+
+//    Constructor
+    public CurrentWeatherResponseEvent(String response) {
+        mResponse = response;
+    }
+
+//    Getters
+    /**
+     * Returns current weather JSON data as string
+     * @return String response
+     */
+    public String getResponse() {
+        return mResponse;
+    }
+}
