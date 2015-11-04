17e1d1ce545deeec82d09770eb8b818d06df5b87
Vitaly Sulimov <quarry386@fastmail.com>
2015-11-05 04:12:23 +0500

    Implemented ForecastWeatherResponseEvent

---
+++ app/src/main/java/com/hochland386/luna/bus/ForecastWeatherResponseEvent.java
@@ -0,0 +1,24 @@
+package com.hochland386.luna.bus;
+
+/**
+ * Created by vitaly on 11/5/15.
+ */
+public class ForecastWeatherResponseEvent {
+
+//    Members
+    private String mResponse;
+
+//    Constructor
+    public ForecastWeatherResponseEvent(String response) {
+        mResponse = response;
+    }
+
+//    Getters
+    /**
+     * Returns forecast weather JSON data as string
+     * @return String response
+     */
+    public String getResponse() {
+        return mResponse;
+    }
+}
