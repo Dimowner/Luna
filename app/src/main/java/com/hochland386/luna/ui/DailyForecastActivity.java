45de89617b5a87804611792babe56e39cf218929
Vitaly Sulimov <quarry386@fastmail.com>
2015-11-06 00:05:21 +0500

    Implemented DailyForecastActivity

--- app/src/main/java/com/hochland386/luna/ui/DailyForecastActivity.java
+++ app/src/main/java/com/hochland386/luna/ui/DailyForecastActivity.java
@@ -1,15 +1,280 @@
 package com.hochland386.luna.ui;
 
+import android.Manifest;
 import android.app.ListActivity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.hochland386.luna.R;
+import com.hochland386.luna.adapters.DailyForecastAdapter;
+import com.hochland386.luna.bus.ForecastWeatherFailureEvent;
+import com.hochland386.luna.bus.ForecastWeatherResponseEvent;
+import com.hochland386.luna.bus.LocationChangedEvent;
+import com.hochland386.luna.bus.LocationFailureEvent;
+import com.hochland386.luna.model.DailyWeather;
+import com.hochland386.luna.model.Forecast;
+import com.hochland386.luna.utils.ProvidersChecker;
+import com.hochland386.luna.utils.ResponseParser;
+import com.hochland386.luna.utils.UrlBuilder;
+import com.hochland386.luna.worker.LocationWorker;
+import com.hochland386.luna.worker.NetworkWorker;
+
+import org.json.JSONException;
+
+import de.greenrobot.event.EventBus;
 
 public class DailyForecastActivity extends ListActivity {
+
+//    Constants
+    private final int LOCATION_PERMISSIONS_REQUEST_CODE = 1;
+
+//    Members
+    private LocationWorker mLocationWorker;
+    private NetworkWorker mNetworkWorker;
+    private ProvidersChecker mProvidersChecker;
+    private UrlBuilder mUrlBuilder;
+    private ResponseParser mResponseParser;
+    private Forecast mForecast;
+    private String[] mLocationPermissionsArray = {
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_forecast);
+
+//        Members init
+        mLocationWorker = LocationWorker.getInstance();
+        mNetworkWorker = NetworkWorker.getInstance();
+        mProvidersChecker = ProvidersChecker.getInstance();
+        mUrlBuilder = UrlBuilder.getInstance();
+        mResponseParser = ResponseParser.getInstance();
+        mForecast = new Forecast();
+
+//        Register for events
+        EventBus.getDefault().register(this);
+
+//        Refresh forecast weather when activity first created
+        refreshWeather();
+    }
+
+//    Implements onDestroy lifecycle method
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        EventBus.getDefault().unregister(this);
+    }
+
+//    Implements onItemClick listener
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        DailyWeather dailyWeather = mForecast.getDailyWithIndex(position);
+        String uppercaseSummary = dailyWeather.getSummary()
+                .substring(0, 1)
+                .toUpperCase()
+                + dailyWeather.getSummary()
+                .substring(1);
+        Toast.makeText(
+                DailyForecastActivity.this,
+                uppercaseSummary,
+                Toast.LENGTH_LONG
+        ).show();
+    }
+
+
+//    Private interface
+    /**
+     * Checks for location and network availability and call getWeatherData() if everything is OK
+     */
+    private void refreshWeather() {
+        if (mProvidersChecker.isLocationAndNetworkAvailable(this)) {
+            getWeatherData();
+        } else {
+            Toast.makeText(
+                    this,
+                    getString(R.string.noLocationOrNetworkErrorMessage),
+                    Toast.LENGTH_LONG
+            ).show();
+        }
+    }
+
+    /**
+     * Checks Location status. If LocationWorker.determineUserLocation() never be called and
+     * device don't listening for location updates then call determineUserLocation().
+     *
+     * If LocationWorker.determineUserLocation() was be called at least once and device don't
+     * listening for location updates then assuming that location already available and we can
+     * use it.
+     *
+     * Finally. If device is listening for location updates but there is no location available
+     * then do nothing, just waiting for Location Event.
+     */
+    private void getWeatherData() {
+        if (!mLocationWorker.isDetermineUserLocationTriggered() && !mLocationWorker.isListeningForUpdates()) {
+        /* Checks runtime permissions. Request permissions if it's not already granted */
+            if (ContextCompat.checkSelfPermission(
+                    DailyForecastActivity.this,
+                    mLocationPermissionsArray[0])
+                    != PackageManager.PERMISSION_GRANTED
+                    && ContextCompat.checkSelfPermission(
+                    DailyForecastActivity.this,
+                    mLocationPermissionsArray[1])
+                    != PackageManager.PERMISSION_GRANTED) {
+                /* Warning! No permissions at this point of the time. Requesting ... */
+                ActivityCompat.requestPermissions(
+                        DailyForecastActivity.this,
+                        mLocationPermissionsArray,
+                        LOCATION_PERMISSIONS_REQUEST_CODE
+                );
+            } else {
+            /* Yay! Required Permissions available. We can proceed */
+                determineUserLocation();
+            }
+        } else if (mLocationWorker.isDetermineUserLocationTriggered() && !mLocationWorker.isListeningForUpdates()) {
+            downloadForecastWeatherData();
+        } else {
+            Toast.makeText(
+                    this,
+                    R.string.waitingForLocationMessage,
+                    Toast.LENGTH_LONG
+            ).show();
+        }
+    }
+
+    /**
+     * Checks availability of location services and request location updates from LocationWorker
+     * if everything is OK, otherwise shows toast with error
+     */
+    private void determineUserLocation() {
+        if (mProvidersChecker.isLocationEnabled(this)) {
+            mLocationWorker.determineUserLocation(this);
+        } else {
+            Toast.makeText(
+                    this,
+                    getString(R.string.noLocationOrNetworkErrorMessage),
+                    Toast.LENGTH_LONG
+            ).show();
+        }
+    }
+
+    /**
+     * Checks availability of network, build forecast weather URL with data from LocationWorker
+     * and download forecast weather data from server. Toast with error message will be shown
+     * if network are unavailable
+     */
+    private void downloadForecastWeatherData() {
+        if (mProvidersChecker.isNetworkAvailable(this)) {
+            String forecastWeatherUrl = mUrlBuilder.buildForecastWeatherUrl(
+                    String.valueOf(mLocationWorker.getLatitude()),
+                    String.valueOf(mLocationWorker.getLongitude())
+            );
+            mNetworkWorker.downloadForecastWeatherData(forecastWeatherUrl);
+        } else {
+            Toast.makeText(
+                    this,
+                    getString(R.string.noLocationOrNetworkErrorMessage),
+                    Toast.LENGTH_LONG
+            ).show();
+        }
+    }
+
+    /**
+     * Fills list with data from mForecast object
+     */
+    private void updateUi() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                DailyForecastAdapter forecastAdapter = new DailyForecastAdapter(
+                        DailyForecastActivity.this,
+                        mForecast
+                );
+                setListAdapter(forecastAdapter);
+            }
+        });
+    }
+
+//    Handle requestPermissions results
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_PERMISSIONS_REQUEST_CODE:
+                /* If request is cancelled, the result arrays are empty. */
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    /* Permissions was granted, yay! */
+                    determineUserLocation();
+                } else {
+                    /* Permissions denied! Shows toast with error */
+                    Toast.makeText(
+                            DailyForecastActivity.this,
+                            getString(R.string.permissionsDeniedErrorMessage),
+                            Toast.LENGTH_LONG
+                    ).show();
+                }
+                break;
+        }
+    }
+
+//    Handle Location events
+    /**
+     * Call downloadCurrentWeatherData() when location determined
+     */
+    public void onEvent(LocationChangedEvent ev) {
+        downloadForecastWeatherData();
+    }
+
+    /**
+     * Call downloadCurrentWeatherData() even if location won't determined. This will get weather
+     * data for last known location or for default location if there is no last known location at all
+     */
+    public void onEvent(LocationFailureEvent ev) {
+        /* Shows toast with error and download weather data for default location */
+        Toast.makeText(
+                this,
+                ev.getFailureMessage(),
+                Toast.LENGTH_LONG
+        ).show();
+        downloadForecastWeatherData();
+    }
+
+//    Handle Network events
+    /**
+     * Trying to parse JSON data and build Forecast object. Call updateUi() when Forecast
+     * object is ready.
+     */
+    public void onEvent(ForecastWeatherResponseEvent ev) {
+        String response = ev.getResponse();
+        try {
+            mForecast = mResponseParser.parseForecastWeatherResponse(response);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(
+                    this,
+                    getString(R.string.jsonExceptionErrorMessage),
+                    Toast.LENGTH_LONG
+            ).show();
+        } finally {
+            updateUi();
+        }
+    }
+
+    /**
+     * Call updateUi() if network failure occurs. This will shows data from mock Forecast
+     * object
+     */
+    public void onEvent(ForecastWeatherFailureEvent ev) {
+        Toast.makeText(
+                this,
+                getString(R.string.networkFailureErrorMessage),
+                Toast.LENGTH_LONG
+        ).show();
+        updateUi();
     }
 }
