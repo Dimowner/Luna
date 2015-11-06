6fff70c4718a6e9cddcbc0694cd62788d5b62c5a
Vitaly Sulimov <quarry386@fastmail.com>
2015-11-06 23:36:24 +0500

    Constants: Implemented singleton pattern

--- app/src/main/java/com/hochland386/luna/utils/Constants.java
+++ app/src/main/java/com/hochland386/luna/utils/Constants.java
@@ -6,15 +6,80 @@ package com.hochland386.luna.utils;
  */
 public class Constants {
 
-//    Public static final fields
-    public static final int LOCATION_TIMEOUT = 45000; /* milliseconds */
-    public static final int COUNT_DOWN_INTERVAL = 1000; /* milliseconds */
-    public static final long LOCATION_MIN_TIME = 0;
-    public static final float LOCATION_MIN_DISTANCE = 0;
-    public static final String API_KEY = "7afd086976468949384e4359c0409420";
-    public static final String FORECAST_COUNT = "10";
+//    Members
+    private int mLocationTimeout;
+    private int mCountDownInterval;
+    private long mLocationMinTime;
+    private float mLocationMinDistance;
+    private String mApiKey;
+    private String mForecastCount;
 
 //    Make default constructor private
     private Constants() {
+        mLocationTimeout = 45000;
+        mCountDownInterval = 1000;
+        mLocationMinTime = 0;
+        mLocationMinDistance = 0;
+        mApiKey = "7afd086976468949384e4359c0409420";
+        mForecastCount = "10";
+    }
+
+//    Singleton wrapper
+    private static class Loader {
+        static Constants instance = new Constants();
+    }
+
+//    Implements getInstance() method
+    public static Constants getInstance() {
+        return Loader.instance;
+    }
+
+//    Public interface
+    /**
+     * Returns default location timeout in milliseconds
+     * @return int locationTimeout
+     */
+    public int getLocationTimeout() {
+        return mLocationTimeout;
+    }
+
+    /**
+     * Returns default count down interval in milliseconds
+     * @return int countDownInterval
+     */
+    public int getCountDownInterval() {
+        return mCountDownInterval;
+    }
+
+    /**
+     * Returns default location min time
+     * @return long locationMinTime
+     */
+    public long getLocationMinTime() {
+        return mLocationMinTime;
+    }
+
+    /**
+     * Returns default location min distance
+     * @return float locationMinDistance
+     */
+    public float getLocationMinDistance() {
+        return mLocationMinDistance;
+    }
+
+    /**
+     * Returns API key for signed request as string
+     * @return String apiKey
+     */
+    public String getApiKey() {
+        return mApiKey;
+    }
+
+    /**
+     * Returns default days count for forecast request
+     * @return String forecastCount
+     */
+    public String getForecastCount() {
+        return mForecastCount;
     }
 }
