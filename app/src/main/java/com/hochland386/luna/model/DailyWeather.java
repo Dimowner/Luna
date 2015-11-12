aae5b6a206974cd288afe6d9b1d7f97bd5061567
Vitaly Sulimov <quarry386@fastmail.com>
2015-11-12 05:32:37 +0500

    DailyWeather: Implemented Parcelable interface

--- app/src/main/java/com/hochland386/luna/model/DailyWeather.java
+++ app/src/main/java/com/hochland386/luna/model/DailyWeather.java
@@ -1,9 +1,12 @@
 package com.hochland386.luna.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by vitaly on 10/28/15.
  */
-public class DailyWeather extends Weather {
+public class DailyWeather extends Weather implements Parcelable {
 
     //    DailyWeather-specific members
     private long mTimeStamp;
@@ -42,4 +45,44 @@ public class DailyWeather extends Weather {
     public String getConditionGroup() {
         return mConditionGroup;
     }
+
+//    Implements Parcelable interface methods
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(getTemperature());
+        dest.writeInt(getHumidity());
+        dest.writeInt(getPressure());
+        dest.writeString(getSummary());
+        dest.writeLong(mTimeStamp);
+        dest.writeString(mConditionGroup);
+    }
+
+//    Private constructor for Parcel data
+    private DailyWeather(Parcel in) {
+        this(
+                in.readInt(),
+                in.readInt(),
+                in.readInt(),
+                in.readString(),
+                in.readLong(),
+                in.readString()
+        );
+    }
+
+    public static final Creator<DailyWeather> CREATOR = new Creator<DailyWeather>() {
+        @Override
+        public DailyWeather createFromParcel(Parcel source) {
+            return new DailyWeather(source);
+        }
+
+        @Override
+        public DailyWeather[] newArray(int size) {
+            return new DailyWeather[size];
+        }
+    };
 }
