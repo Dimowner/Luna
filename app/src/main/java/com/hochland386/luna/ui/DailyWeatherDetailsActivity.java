290d1d80a0a33746d04294e89d231aa42fc28f8a
Vitaly Sulimov <quarry386@fastmail.com>
2015-11-12 06:46:40 +0500

    DailyWeatherDetailsActivity milestone 1 Implemented base layout

---
+++ app/src/main/java/com/hochland386/luna/ui/DailyWeatherDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.hochland386.luna.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hochland386.luna.R;
+
+public class DailyWeatherDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_weather_details);
+    }
+}
